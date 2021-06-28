b51a7ad348d26a0a9009df6d245aef06f2a57a4b
Enmanuel Reyes <enmanuelmreyes@gmail.com>
2021-06-27 20:41:33 -0400

    last

--- stopwatchApp/src/App.js
+++ stopwatchApp/src/App.js
@@ -199,7 +199,7 @@ const App = () => {
                                     {
                                         LapDetails.map((detail) => {
                                             return (
-                                                < tr key={detail.id} className={detail.hihg == true ? 'hi' : detail.low == true ? 'lo' : ''}  >
+                                                < tr key={detail.id} className={detail.hihg == true ? 'high' : detail.low == true ? 'low' : ''}  >
                                                     <td>{detail.id}</td>
                                                     <td>{detail.lapTIme}</td>
                                                     <td>{detail.overallTime}</td>
